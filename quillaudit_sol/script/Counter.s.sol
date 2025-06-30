c034a8d7536422a6a490776e625f0ef7cf296ca6
abhinabphy <d.abhinab@iitg.ac.in>
2025-06-30 23:19:03 +0530

    added factory solutions

---
+++ quillaudit_sol/script/Counter.s.sol
@@ -0,0 +1,19 @@
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.13;
+
+// import {Script, console} from "forge-std/Script.sol";
+// import {CurvePool} from "../src/Curvepool.sol";
+
+// contract CounterScript is Script {
+//     CurvePool public pool;
+
+//     function setUp() public {}
+
+//     function run() public {
+//         vm.startBroadcast();
+
+//         pool = new CurvePool();
+
+//         vm.stopBroadcast();
+//     }
+// }
